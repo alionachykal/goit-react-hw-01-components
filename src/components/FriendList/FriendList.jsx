955265e927e3e5a9b99be4cdb2c303a17a674ec9
alionachykal <106018567+alionachykal@users.noreply.github.com>
2022-12-17 17:25:10 +0300

    Update FriendList.jsx
    fixing mistakes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,16 +5,16 @@ import { FriendListItem } from './FriendListItem';
 
 
 export const FriendList = ({ friends }) => (
-  <ul className={css.friendList}>
-    {friends.map(friend => (
-      <FriendListItem>
-        key={friend.id}
-        avatar={friend.avatar}
-        isOnline={friend.isOnline}
-        name={friend.name}
-      </FriendListItem>
-    ))}
-  </ul>
+  <ul className={css.friendList}> 
+        {friends.map(({id,avatar,name,isOnline}) => ( 
+        <FriendListItem 
+        key={id} 
+        avatar={avatar} 
+        name={name} 
+        isOnline={isOnline} 
+        />   
+    ))} 
+    </ul>
 );
 
 FriendList.propTypes = {
